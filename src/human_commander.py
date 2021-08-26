41aca98ef41fe5e2670927245b55475434cc004b
HasegawaShoichi <0ve38x163040p4z@ezweb.ne.jp>
2021-08-27 02:47:10 +0900

    Created human commander

--- src/human_commander.py
+++ src/human_commander.py
@@ -6,8 +6,8 @@
 import rospy
 from std_msgs.msg import String
 import random
-import os
-import sys
+#import os
+#import sys
 
 #sys.path.append(os.path.dirname(os.path.abspath(__file__)) + "/../" + "../")
 #sys.path.append('/root/RULO/catkin_ws/src/problog_ros/src')
@@ -17,8 +17,9 @@ import sys
 
 class EnterCommand():
     def __init__(self):
-        self.pub = rospy.Publisher('/human_command', String, queue_size=10) ## queue size is not important for sending just one messeage.
+        self.pub = rospy.Publisher('/human_command', String, queue_size=1) ## queue size is not important for sending just one messeage.
         #self.problog_code = global_knowledge.LogicalInference()
+        #self._base_time = rospy.Time.now().to_sec()
         self.name = ["blue_cup", "green_cup", "orange_cup",
                     "penguin_doll", "pig_doll", "sheep_doll",
                     "coffee_bottle", "fruits_bottle", "muscat_bottle"]
@@ -45,26 +46,19 @@ class EnterCommand():
 
     def StartPublish(self): 
         ## node initialization
-        rate = rospy.Rate(1) # 1 Hz
+        #rate = rospy.Rate(1) # 1 Hz
         n = random.randint(0, len(self.name)-1)
         #while not rospy.is_shutdown():
         #TeachingText = raw_input('Enter human command: ')               
         TeachingText = self.name[n]
+        print(TeachingText)
         print('Command: ' + 'Bring ' + TeachingText + ' for me')
-        #result = self.problog_code(TeachingText)
-        #print(result)
-        #str_msg = String(data= TeachingText )
-        #rospy.loginfo('%s publish %s'%(rospy.get_name(),str_msg.data))
-        #self.pub.publish(str_msg)
-        rate.sleep()
-
+        #while rospy.Time.now().to_sec() - self._base_time < 10:
+        while not rospy.is_shutdown():
+            self.pub.publish(TeachingText)
 
 if __name__ == '__main__':
     rospy.init_node('enter_human_command', anonymous=False)
-    try:
-        ## This function might be made as a sevice of ROS for stable work.
-        ## Use publisher function tentatively,because it is easy.
-        #CallService()
-        enter = EnterCommand()
-        enter.StartPublish()
-    except rospy.ROSInterruptException: pass
+    enter = EnterCommand()
+    enter.StartPublish()
+    rospy.spin()
